ace7c3595931ab1fcbca13f94d564fb09e3e39d6
zhanxw <zhanxw@gmail.com>
2015-01-07 19:35:50 -0500

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ cd ..
 if [[ -f .emacs || -d emacs || .emacs.d ]]; then
   echo "Emacs setting exists, skipping..."
 else
-    ln -s dotfiles/emacs/.emacs
-    ln -s dotfiles/emacs/emacs
-    ln -s dotfiles/emacs/.emacs.d
+    ln -s dotFiles/emacs/.emacs
+    ln -s dotFiles/emacs/emacs
+    ln -s dotFiles/emacs/.emacs.d
 fi
 # Screen
 if [[ -f .screenrc ]]; then
     echo "Screen setting exists, skipping..."
 else
-    ln -s dotfiles/screen/.screenrc
+    ln -s dotFiles/screen/.screenrc
 fi
 
 cd -
