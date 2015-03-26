1f5b514c55c9f3d67abc4b242e74f034bbb34faa
zhanxw <zhanxw@gmail.com>
2015-03-26 13:20:48 -0400

    update emacs packages

---
+++ emacs/.emacs.d/elpa/clang-format-20150119.248/clang-format-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clang-format" "20150119.248" "Format code using clang-format" '((cl-lib "0.3")) :keywords '("tools" "c"))
--- emacs/.emacs.d/elpa/ivy-20150315.708/ivy-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ivy" "20150315.708" "Incremental Vertical completYon" '((emacs "24.1")) :url "https://github.com/abo-abo/swiper" :keywords '("matching"))
