3c27f73d7181d3e018dd1812bc9d510843a75eb3
zhanxw <zhanxw@gmail.com>
2016-10-28 15:51:09 -0400

    update Emacs packages

---
+++ emacs/.emacs.d/elpa/bm-20161024.1006/bm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bm" "20161024.1006" "Visible bookmarks in buffer." 'nil :url "https://github.com/joodland/bm" :keywords '("bookmark" "highlight" "faces" "persistent"))
---
+++ emacs/.emacs.d/elpa/clang-format-20161004.253/clang-format-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clang-format" "20161004.253" "Format code using clang-format" '((cl-lib "0.3")) :keywords '("tools" "c"))
