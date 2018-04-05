4ff868c8bf4cc6e41ac575a81f884057d0f6fd39
zhanxw <zhanxw@gmail.com>
2018-04-05 10:16:48 -0500

    update emacs packages

---
+++ emacs/.emacs.d/elpa/auto-package-update-20170803.1005/auto-package-update-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-package-update" "20170803.1005" "Automatically update Emacs packages." '((emacs "24.4") (dash "2.1.0")) :commit "0b296970d09d7deb7f780cb041fcbc313d39aa5d" :url "http://github.com/rranelli/auto-package-update.el" :keywords '("package" "update"))
