ecc3785ea5fb4bc1da550cb88f5e0a80faf8b0ee
zhanxw <zhanxw@gmail.com>
2018-07-03 16:33:50 -0500

    update emacs packages

---
+++ emacs/.emacs.d/elpa/anaphora-20180618.1500/anaphora-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "anaphora" "20180618.1500" "anaphoric macros providing implicit temp variables" 'nil :commit "3b2da3f759b244975852e79721c4a2dbad3905cf" :keywords '("extensions") :url "http://github.com/rolandwalker/anaphora")
